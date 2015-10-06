c1c232dfa486daf7821d4992eb44068c73f4ab6d
akshat1 <akshat.sh@gmail.com>
2015-10-06 01:56:04 -0700

    - Better environment detection - gulpfile cleanup - jsdoc

--- dist/compute-debug.js
+++ dist/compute-debug.js
@@ -1,10 +1,17 @@
-(function() {
-  var C, MSGInvalidArgumentsToObservableArray, Observable, ObservableArray, err, ko, _gather, _isObservable, _isValid, _unwrap,
-    __slice = [].slice;
 
-  C = {
+/**
+ * @namespace Compute
+ */
+
+(function() {
+  var C, Compute, MSGInvalidArgumentsToObservableArray, Observable, ObservableArray, _gather, _isValid, _unwrap, err, error, ko,
+    slice = [].slice;
+
+  Compute = {
     version: '0.0.7'
   };
+
+  C = Compute;
 
   MSGInvalidArgumentsToObservableArray = 'The argument passed when initializing an observable array must be an array, or null, or undefined.';
 
@@ -17,37 +24,52 @@
   Also, try to grab knockout from the window (if present), or
   require('knockout') if in node. Silently fall back to using our own
   observables if knockout is not installed.
+  
+  Many thanks to knockoutjs for the environment determination logic
    */
 
-  if (typeof window !== 'undefined') {
-    window.Compute = C;
-    ko = window.ko;
-  } else if (module) {
+  if (typeof require === 'function' && typeof (typeof module !== "undefined" && module !== null ? module.exports : void 0) === 'object' && typeof module === 'object') {
     module.exports = C;
     try {
       ko = require('knockout');
-    } catch (_error) {
-      err = _error;
+    } catch (error) {
+      err = error;
 
       /*
       KO not found. We'll eat the exception and use our own observables
       implementation.
        */
     }
+  } else if (typeof window !== 'undefined') {
+    window.Compute = C;
+    ko = window.ko;
+  } else if (typeof define === 'function' && define['amd']) {
+    define(function() {
+      return C;
+    });
+  } else {
+    throw new Error('Compute can not identify this environment. Please let the developers know at https://github.com/akshat1/compute/issues');
   }
 
+
+  /**
+   * compute's implementation of an Observable
+   * @param val - the value of the observable
+   * @return {Observable}
+   */
+
   Observable = function(val) {
-    var callSubscribers, o, subscriptions, _value;
+    var _subscriptions, _value, callSubscribers, o;
     _value = val;
-    subscriptions = [];
+    _subscriptions = [];
     callSubscribers = function() {
-      var s, _i, _len, _results;
-      _results = [];
-      for (_i = 0, _len = subscriptions.length; _i < _len; _i++) {
-        s = subscriptions[_i];
-        _results.push(s(_value));
+      var i, len, results, s;
+      results = [];
+      for (i = 0, len = _subscriptions.length; i < len; i++) {
+        s = _subscriptions[i];
+        results.push(s(_value));
       }
-      return _results;
+      return results;
     };
     o = function(newVal) {
       var isValueChanged;
@@ -62,27 +84,34 @@
       }
     };
     o.subscribe = function(f) {
-      return subscriptions.push(f);
+      return _subscriptions.push(f);
     };
     o._isObservable = true;
     return o;
   };
 
+
+  /**
+   * compute's implementation of an Observable Array
+   * @param val - the value of the observable
+   * @return {Observable}
+   */
+
   ObservableArray = function(arr) {
-    var callSubscribers, o, subscriptions, _value;
+    var _subscriptions, _value, callSubscribers, o;
     _value = arr || [];
     if (!(_value instanceof Array)) {
       throw new Error(MSGInvalidArgumentsToObservableArray);
     }
-    subscriptions = [];
+    _subscriptions = [];
     callSubscribers = function() {
-      var s, _i, _len, _results;
-      _results = [];
-      for (_i = 0, _len = subscriptions.length; _i < _len; _i++) {
-        s = subscriptions[_i];
-        _results.push(s(_value));
+      var i, len, results, s;
+      results = [];
+      for (i = 0, len = _subscriptions.length; i < len; i++) {
+        s = _subscriptions[i];
+        results.push(s(_value));
       }
-      return _results;
+      return results;
     };
     o = function(newArr) {
       if (arguments.length > 0) {
@@ -93,7 +122,7 @@
       }
     };
     o.subscribe = function(f) {
-      return subscriptions.push(f);
+      return _subscriptions.push(f);
     };
     o.push = function(v) {
       _value.push(v);
@@ -109,7 +138,14 @@
     return o;
   };
 
-  _isObservable = function(v) {
+
+  /**
+   * @param {Object} v
+   * @returns {Boolean} - Whether or not v is an observable
+   * @memberof Compute
+   */
+
+  C.isObservable = function(v) {
     if (typeof ko !== 'undefined') {
       return ko.isObservable(v);
     } else {
@@ -117,23 +153,39 @@
     }
   };
 
+
+  /**
+   * @param {Observable|ObservableArray} v
+   * @returns - The value of the observable
+   */
+
   _unwrap = function(v) {
-    if (_isObservable(v)) {
+    if (C.isObservable(v)) {
       return v();
     } else {
       return v;
     }
   };
 
+
+  /**
+   * Make sure that all observables (for C.on or C.from) are observables and 
+   * that the function is a function.
+   * @param {Array} observables - The array of observables
+   * @param {function} func - The function to be executed
+   * @returns {boolean} - Whether or not all observables really are observable 
+   *                      and func really is a function.
+   */
+
   _isValid = function(observables, func) {
-    var o, _i, _len;
-    for (_i = 0, _len = observables.length; _i < _len; _i++) {
-      o = observables[_i];
-      if (!_isObservable(o)) {
+    var i, len, o;
+    for (i = 0, len = observables.length; i < len; i++) {
+      o = observables[i];
+      if (!C.isObservable(o)) {
         return false;
       }
     }
-    if (_isObservable(func)) {
+    if (C.isObservable(func)) {
       return false;
     }
     if (typeof func !== 'function') {
@@ -142,23 +194,33 @@
     return true;
   };
 
+
+  /**
+   * collect the value of all observables into an Array
+   * @param {Array} observables - An array of observables
+   * @returns {Array} - The value of those observables
+   */
+
   _gather = function(observables) {
-    var o, values, _i, _len;
+    var i, len, o, values;
     values = [];
-    for (_i = 0, _len = observables.length; _i < _len; _i++) {
-      o = observables[_i];
+    for (i = 0, len = observables.length; i < len; i++) {
+      o = observables[i];
       values.push(_unwrap(o));
     }
     return values;
   };
 
 
-  /*
-  Usage:
-    x = Compute.o 'foo'
-    console.log x()      //foo
-    x 'bar'
-    console.log x()      //bar
+  /**
+   * Usage:
+   *   x = Compute.o 'foo'
+   *   console.log x()      //foo
+   *   x 'bar'
+   *   console.log x()      //bar
+   * @param val - value of the observable
+   * @returns {Observable}
+   * @memberof Compute
    */
 
   C.o = function(val) {
@@ -170,12 +232,15 @@
   };
 
 
-  /*
-  Usage:
-    x = Compute.o ['foo', 'bar']
-    console.log x()      //['foo', 'bar']
-    x.push 'bar'
-    console.log x()      //['foo', 'bar', 'baz']
+  /**
+   * Usage:
+   *   x = Compute.o ['foo', 'bar']
+   *   console.log x()      //['foo', 'bar']
+   *   x.push 'bar'
+   *   console.log x()      //['foo', 'bar', 'baz']
+   * @param {Array} arr - value of the observable
+   * @returns {ObservableArray}
+   * @memberof Compute
    */
 
   C.oa = function(arr) {
@@ -187,25 +252,43 @@
   };
 
 
-  /*
-  Execute function 'f' when any of the observables change. Pass observable values
-  as arguments.
-  
-  Usage:
-    C.on obs1, obs2, obs3, (o1, o2, o3)->
-      console.log "One of the observables changed!"
-  
-  grab the on like so
-    t = C.on .....
-  
-  Call t.$stop() to ignore subsequent observable mutations. Call t.$resume() to
-  resume. Call $fire() to force executing f with current values of all
-  observables.
+  /**
+   * <p>
+   * Execute function 'f' when any of the observables change. Pass observable values
+   * as arguments.
+   * </p><p>
+   * Usage:
+   * <pre><code>
+   * function onWatchedObservableChanged(v1, v2, v3) {
+   *   console.log("One of the observables changed! We can do something");
+   *   alert("An observable changed");
+   * }
+   * var watch = C.on(obs1, obs2, obs3, onWatchedObservableChanged);
+   * 
+   * //Stop watching
+   * watch.$stop()
+   * 
+   * //Resume watching
+   * watch.$resume()
+   *
+   * //Force execution
+   * watch.$fire()
+   * 
+   * </code></pre>
+   * </p><p>
+   * @param {...Observable} - the observables to be watched
+   * @param {function} f - the function used to handle the change. Should expect
+   *                       the value of each observable as an argument and return
+   *                       some value to be stored in the resultant observable.
+   *                       So, for N watched observables, f must expect N 
+   *                       arguments
+   * @returns {Object} - An object just containing $stop, $fire and $resume functions
+   * @memberof Compute
    */
 
   C.on = function() {
-    var f, func, isStopped, o, observables, _i, _j, _len;
-    observables = 2 <= arguments.length ? __slice.call(arguments, 0, _i = arguments.length - 1) : (_i = 0, []), f = arguments[_i++];
+    var f, func, i, isStopped, j, len, o, observables;
+    observables = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), f = arguments[i++];
     if (!_isValid(observables, f)) {
       throw new Error('Invalid arguments to C.on');
     }
@@ -216,8 +299,8 @@
       }
       return f.apply(null, _gather(observables));
     };
-    for (_j = 0, _len = observables.length; _j < _len; _j++) {
-      o = observables[_j];
+    for (j = 0, len = observables.length; j < len; j++) {
+      o = observables[j];
       o.subscribe(func);
     }
     return {
@@ -232,27 +315,43 @@
   };
 
 
-  /*
-  Define a new observable, whose value is the value returned by the function 'f'
-  when 'f' called with the values of all observables.
-  
-  This new observable is updated every time one of the observables mutate.
-  
-  Usage:
-    C.on obs1, obs2, obs3, (o1, o2, o3)->
-      console.log "One of the observables changed!"
-  
-  grab the on like so
-    t = C.on .....
-  
-  Call t.$stop() to ignore subsequent observable mutations. Call t.$resume() to
-  resume. Call $fire() to force executing f with current values of all
-  observables.
+  /**
+   * <p>
+   * Define a new observable, whose value is the value returned by the function 'f'
+   * when 'f' is called with the values of all observables.
+   * </p><p>
+   * This new observable is updated every time one of the observables mutate.
+   * </p><p>
+   * Note that for some reason I can't get JSDoc to show the varargs in the documentation
+   * </p><p>
+   * Usage:
+   * <pre><code>
+   * var newObservable = C.on(obs1, obs2, obs3, function (v1, v2, v3){
+   *    console.log "One of the observables changed! We can calculate a result using their values";
+   *    return v1 + v2 + v2;
+   *    });
+   *
+   * //Stop watching
+   * newObservable.$stop()
+   * 
+   * //Resume watching
+   * newObservable.$resume()
+   *
+   * //Force re-eval
+   * newObservable.$fire()
+   * </code></pre>
+   * </p>
+   * @param {...Observable} - the observables to be watched
+   * @param {function} f - the function used to calculate the result. Should expect
+   *                       the value of each observable as an argument. So, for N
+   *                       watched observables, f must expect N arguments
+   * @returns {Observable} - The observable obtained by executing f(...ValueOfObservables)
+   * @memberof Compute
    */
 
   C.from = function() {
-    var f, func, isStopped, newOb, o, observables, _i, _j, _len;
-    observables = 2 <= arguments.length ? __slice.call(arguments, 0, _i = arguments.length - 1) : (_i = 0, []), f = arguments[_i++];
+    var f, func, i, isStopped, j, len, newOb, o, observables;
+    observables = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), f = arguments[i++];
     if (!_isValid(observables, f)) {
       throw new Error('Invalid arguments to C.from');
     }
@@ -267,8 +366,8 @@
       newOb(val);
       return newOb;
     };
-    for (_j = 0, _len = observables.length; _j < _len; _j++) {
-      o = observables[_j];
+    for (j = 0, len = observables.length; j < len; j++) {
+      o = observables[j];
       o.subscribe(func);
     }
     newOb.$fire = func;
@@ -284,8 +383,6 @@
   C._gather = _gather;
 
   C._isValid = _isValid;
-
-  C._isObservable = _isObservable;
 
   C._unwrap = _unwrap;
 
--- src/Compute.coffee
+++ src/Compute.coffee
@@ -1,5 +1,11 @@
-C =
+###*
+# @namespace Compute
+###
+
+Compute = 
   version : '%%%COMPUTE_VERSION%%%'
+
+C = Compute
 
 
 MSGInvalidArgumentsToObservableArray = 'The argument passed when initializing an observable array must be an array, or null, or undefined.'
@@ -14,13 +20,11 @@ module.exports.
 Also, try to grab knockout from the window (if present), or
 require('knockout') if in node. Silently fall back to using our own
 observables if knockout is not installed.
-###
-if typeof window isnt 'undefined'
-  window.Compute = C
-  ko = window.ko
-else if module
-  module.exports = C
 
+Many thanks to knockoutjs for the environment determination logic
+###
+if typeof require is 'function' and typeof module?.exports is 'object' and typeof module is 'object'
+  module.exports = C
   try
     ko = require 'knockout'
   catch err
@@ -29,17 +33,32 @@ else if module
     implementation.
     ###
 
+else if typeof window isnt 'undefined'
+  window.Compute = C
+  ko = window.ko
 
-## Our custom observables implementation.
+else if typeof define is 'function' and define['amd']
+  define () -> C
+
+else
+  throw new Error 'Compute can not identify this environment. Please let the developers know at https://github.com/akshat1/compute/issues'
+
+
+# Our custom observables implementation.
 # This will be used if, and only if knockout is not found.
 
+###*
+# compute's implementation of an Observable
+# @param val - the value of the observable
+# @return {Observable}
+###
 Observable = (val) ->
   _value = val
 
-  subscriptions = []
+  _subscriptions = []
 
   callSubscribers = ()->
-    for s in subscriptions
+    for s in _subscriptions
       s _value
 
   o = (newVal)->
@@ -53,21 +72,26 @@ Observable = (val) ->
     else
       _value
 
-  o.subscribe = (f)-> subscriptions.push f
+  o.subscribe = (f)-> _subscriptions.push f
   o._isObservable = true
 
   o
 
 
+###*
+# compute's implementation of an Observable Array
+# @param val - the value of the observable
+# @return {Observable}
+###
 ObservableArray = (arr)->
   _value = arr or []
   unless _value instanceof Array
     throw new Error MSGInvalidArgumentsToObservableArray
 
-  subscriptions = []
+  _subscriptions = []
 
   callSubscribers = ()->
-    for s in subscriptions
+    for s in _subscriptions
       s _value
 
   o = (newArr)->
@@ -78,7 +102,7 @@ ObservableArray = (arr)->
     else
       _value
 
-  o.subscribe = (f)-> subscriptions.push f
+  o.subscribe = (f)-> _subscriptions.push f
 
   o.push = (v)->
     _value.push v
@@ -96,23 +120,46 @@ ObservableArray = (arr)->
 
 ## Internal utility functions
 
-_isObservable = (v)->
+###*
+# @param {Object} v
+# @returns {Boolean} - Whether or not v is an observable
+# @memberof Compute
+###
+C.isObservable = (v)->
   if typeof ko isnt 'undefined'
     ko.isObservable v
   else
     v._isObservable or false
 
 
-_unwrap = (v)-> if _isObservable v then v() else v
+###*
+# @param {Observable|ObservableArray} v
+# @returns - The value of the observable
+###
+_unwrap = (v)-> if C.isObservable v then v() else v
 
 
+###*
+# Make sure that all observables (for C.on or C.from) are observables and 
+# that the function is a function.
+# @param {Array} observables - The array of observables
+# @param {function} func - The function to be executed
+# @returns {boolean} - Whether or not all observables really are observable 
+#                      and func really is a function.
+###
 _isValid = (observables, func)->
   for o in observables
-    return false unless _isObservable(o)
-  return false if _isObservable(func)
+    return false unless C.isObservable(o)
+  return false if C.isObservable(func)
   return false unless typeof func is 'function'
   true
 
+
+###*
+# collect the value of all observables into an Array
+# @param {Array} observables - An array of observables
+# @returns {Array} - The value of those observables
+###
 _gather = (observables)->
   values = []
   values.push _unwrap(o) for o in observables
@@ -122,12 +169,15 @@ _gather = (observables)->
 ## Expose observables to the outside world.
 # This also decides whether to use ko or use Compute's observables.
 
-###
-Usage:
-  x = Compute.o 'foo'
-  console.log x()      //foo
-  x 'bar'
-  console.log x()      //bar
+###*
+# Usage:
+#   x = Compute.o 'foo'
+#   console.log x()      //foo
+#   x 'bar'
+#   console.log x()      //bar
+# @param val - value of the observable
+# @returns {Observable}
+# @memberof Compute
 ###
 C.o = (val)->
   if typeof ko isnt 'undefined'
@@ -136,12 +186,15 @@ C.o = (val)->
     Observable val
 
 
-###
-Usage:
-  x = Compute.o ['foo', 'bar']
-  console.log x()      //['foo', 'bar']
-  x.push 'bar'
-  console.log x()      //['foo', 'bar', 'baz']
+###*
+# Usage:
+#   x = Compute.o ['foo', 'bar']
+#   console.log x()      //['foo', 'bar']
+#   x.push 'bar'
+#   console.log x()      //['foo', 'bar', 'baz']
+# @param {Array} arr - value of the observable
+# @returns {ObservableArray}
+# @memberof Compute
 ###
 C.oa = (arr)->
   if typeof ko isnt 'undefined'
@@ -150,20 +203,38 @@ C.oa = (arr)->
     ObservableArray arr
 
 
-###
-Execute function 'f' when any of the observables change. Pass observable values
-as arguments.
-
-Usage:
-  C.on obs1, obs2, obs3, (o1, o2, o3)->
-    console.log "One of the observables changed!"
-
-grab the on like so
-  t = C.on .....
-
-Call t.$stop() to ignore subsequent observable mutations. Call t.$resume() to
-resume. Call $fire() to force executing f with current values of all
-observables.
+###*
+# <p>
+# Execute function 'f' when any of the observables change. Pass observable values
+# as arguments.
+# </p><p>
+# Usage:
+# <pre><code>
+# function onWatchedObservableChanged(v1, v2, v3) {
+#   console.log("One of the observables changed! We can do something");
+#   alert("An observable changed");
+# }
+# var watch = C.on(obs1, obs2, obs3, onWatchedObservableChanged);
+# 
+# //Stop watching
+# watch.$stop()
+# 
+# //Resume watching
+# watch.$resume()
+#
+# //Force execution
+# watch.$fire()
+# 
+# </code></pre>
+# </p><p>
+# @param {...Observable} - the observables to be watched
+# @param {function} f - the function used to handle the change. Should expect
+#                       the value of each observable as an argument and return
+#                       some value to be stored in the resultant observable.
+#                       So, for N watched observables, f must expect N 
+#                       arguments
+# @returns {Object} - An object just containing $stop, $fire and $resume functions
+# @memberof Compute
 ###
 C.on = (observables..., f)->
   unless _isValid observables, f
@@ -180,22 +251,38 @@ C.on = (observables..., f)->
   $resume : ()-> isStopped = false
 
 
-###
-Define a new observable, whose value is the value returned by the function 'f'
-when 'f' called with the values of all observables.
-
-This new observable is updated every time one of the observables mutate.
-
-Usage:
-  C.on obs1, obs2, obs3, (o1, o2, o3)->
-    console.log "One of the observables changed!"
-
-grab the on like so
-  t = C.on .....
-
-Call t.$stop() to ignore subsequent observable mutations. Call t.$resume() to
-resume. Call $fire() to force executing f with current values of all
-observables.
+###*
+# <p>
+# Define a new observable, whose value is the value returned by the function 'f'
+# when 'f' is called with the values of all observables.
+# </p><p>
+# This new observable is updated every time one of the observables mutate.
+# </p><p>
+# Note that for some reason I can't get JSDoc to show the varargs in the documentation
+# </p><p>
+# Usage:
+# <pre><code>
+# var newObservable = C.on(obs1, obs2, obs3, function (v1, v2, v3){
+#    console.log "One of the observables changed! We can calculate a result using their values";
+#    return v1 + v2 + v2;
+#    });
+#
+# //Stop watching
+# newObservable.$stop()
+# 
+# //Resume watching
+# newObservable.$resume()
+#
+# //Force re-eval
+# newObservable.$fire()
+# </code></pre>
+# </p>
+# @param {...Observable} - the observables to be watched
+# @param {function} f - the function used to calculate the result. Should expect
+#                       the value of each observable as an argument. So, for N
+#                       watched observables, f must expect N arguments
+# @returns {Observable} - The observable obtained by executing f(...ValueOfObservables)
+# @memberof Compute
 ###
 C.from = (observables..., f)->
   unless _isValid observables, f
@@ -214,11 +301,11 @@ C.from = (observables..., f)->
   newOb.$resume = ()-> isStopped = false
   newOb
 
+
 ## Export everything on Compute
 # (Export internal util functions too, for testing).
 C._gather         = _gather
 C._isValid        = _isValid
-C._isObservable   = _isObservable
 C._unwrap         = _unwrap
 C.Observable      = Observable
 C.ObservableArray = ObservableArray
